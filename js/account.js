ee00177ac742b840050dfa191475ff0e414f1e70
CommanderAlchemy <commander.alchemy@gmail.com>
2014-01-07 00:11:01 +0100

    Somewhat ready account information code, only a loop needed to iterate on my TODO LIST

--- js/account.js
+++ js/account.js
@@ -3,17 +3,50 @@ getAccountStatus();
 function getAccountStatus(){
 	var keyID	 	= localStorage.getItem("keyID");
 	var vCode	 	= localStorage.getItem("vCode");
-	var characterID	= $("#characterDropdown").val();
+	var characters = JSON.parse(localStorage.getItem("characters"));
+    var accountStatus = [];
 
+
+
+    /* Create loop that loops through all characters in localstorage and get information
+     * $.each(characters)
+     */
+
+    alert(characters[i].Name + " " + characters[i].Id);
 	$.ajax({
 		url: "server.php",
-		data: {type: "combat", key: keyID, code: vCode, char: characterID},
+		data: {type: "getAccountStatus", key: keyID, code: vCode, char: characters[i].Id},
 		dataType: "xml"
 	}).done(function(data){
 		$(data).find('result').each(function(){
-			alert($(this));
-		});
-	}).fail(function(){
+
+            // Query information
+            var paidUntil 		= $(this).find("paidUntil").text(),
+				createDate 		= $(this).find("createDate").text(),
+				logonCount 	    = $(this).find("logonCount").text(),
+				logonMinutes    = $(this).find("logonMinutes").text(),
+                cachedUntil     = $(this).find("cachedUntil").text();
+
+            // Create object for each character
+            var accountObject   = { "CharID"        :   characters[i].Id,
+                                    "paidUntil"     :   paidUntil,
+                                    "createDate"    :   createDate,
+                                    "logonCount"    :   logonCount,
+                                    "logonMinutes"  :   logonMinutes,
+                                    "cachedUntil"   :   cachedUntil };
+
+            accountStatus.push(accountObject);
+    });
+
+    }).fail(function(){
 		alert("Misslyckades att hämta konto information.");
 	});
+
+    /*
+     * Create JSON object and put into localstorage
+     */
+    var accounts = JSON.stringify(accountStatus);
+    localStorage.setItem('Accounts', accounts);
+    alert(chars);
+
 }
