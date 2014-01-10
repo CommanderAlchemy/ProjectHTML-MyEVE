357008f6ced100489bfab4b7c56a9759084f1fd7
CommanderAlchemy <commander.alchemy@gmail.com>
2014-01-10 17:29:26 +0100

    Accountinformation is now functional

--- js/account.js
+++ js/account.js
@@ -1,52 +1,120 @@
-getAccountStatus();
+getAccountStatus(0);
+/*
+ Get character information for each account.
+ This uses EVE - API : https://api.eveonline.com/account/AccountStatus.xml.aspx?
+    paidUntil       how long the account is paid for.
+    createDate      what date the character was created.
+    logonCount      how many times the character was logged in to the eve account.
+    logonMinutes    how many minutes the character has been logged in.
+*/
 
-function getAccountStatus(){
+// Array that get stored in local storage.
+var accountStatus = [];
+
+/*
+    This function gets accountinformation using the API above and stores the data
+    into a JSON object and then into local storage.
+
+    This function will itterate through all characters located in local storage
+
+    feature: Make application query accounts instead of characters to have information about several accounts.
+*/
+function getAccountStatus(index){
 	var keyID	 	= localStorage.getItem("keyID");
 	var vCode	 	= localStorage.getItem("vCode");
 	var characters = JSON.parse(localStorage.getItem("characters"));
-    var accountStatus = [];
 
-
-
-    /* Create loop that loops through all characters in localstorage and get information
-     * $.each(characters)
-     */
-
-    alert(characters[i].Name + " " + characters[i].Id);
 	$.ajax({
 		url: "server.php",
-		data: {type: "getAccountStatus", key: keyID, code: vCode, char: characters[i].Id},
+		data: {type: "getAccountStatus", key: keyID, code: vCode, char: characters[index].Id},
 		dataType: "xml"
 	}).done(function(data){
-		$(data).find('result').each(function(){
+        var cachedUntil         = $(data).find("cachedUntil").text();
 
-            // Query information
+		$(data).find("result").each(function(){
+            // Query information per character in account
             var paidUntil 		= $(this).find("paidUntil").text(),
 				createDate 		= $(this).find("createDate").text(),
 				logonCount 	    = $(this).find("logonCount").text(),
-				logonMinutes    = $(this).find("logonMinutes").text(),
-                cachedUntil     = $(this).find("cachedUntil").text();
+				logonMinutes    = $(this).find("logonMinutes").text();
 
             // Create object for each character
-            var accountObject   = { "CharID"        :   characters[i].Id,
+            var accountObject   = { "CharID"        :   characters[index].Id,
                                     "paidUntil"     :   paidUntil,
                                     "createDate"    :   createDate,
                                     "logonCount"    :   logonCount,
                                     "logonMinutes"  :   logonMinutes,
                                     "cachedUntil"   :   cachedUntil };
 
+            // Push caracter object into the accountStatus array
+            var character = JSON.stringify(accountObject);
             accountStatus.push(accountObject);
+
+            // If there are more characters left itterate
+            if (index < characters.length -1){
+                getAccountStatus(++index);
+            } else {
+                // When the itteration is done store the whole array in localstorage.
+                var accounts = JSON.stringify(accountStatus);
+                localStorage.setItem("accounts", accounts);
+                alert("accounts" + accounts);
+                loadAccountStatus();
+            }
     });
 
     }).fail(function(){
-		alert("Misslyckades att hämta konto information.");
+		alert("Misslyckades att hämta karaktär information för kontot");
 	});
-
-    /*
-     * Create JSON object and put into localstorage
-     */
-    var accounts = JSON.stringify(accountStatus);
-    localStorage.setItem('Accounts', accounts);
-    alert(chars);
-
 }
+
+/*
+    Load accountinformation into webpage
+*/
+function loadAccountStatus(){
+    var accounts = JSON.parse(localStorage.getItem("accounts"));
+    alert(accounts);
+    var totalPlayTime,
+        paidUntil,
+        logonCount;
+
+/*    for (var i = accounts.length -1; i > 0; i--){
+        totalPlayTime += parseInt(accounts[i].logonMinutes);
+    }*/
+
+    totalPlayTime = parseInt(accounts[0].logonMinutes);
+    paidUntil = accounts[0].paidUntil;
+    logonCount = accounts[0].logonCount;
+
+    // Convert from minutes to...
+    var years = Math.floor(totalPlayTime / 525600);
+    totalPlayTime -= (years*525600);
+
+    var months = Math.floor(totalPlayTime / 43800);
+    totalPlayTime -= (months*43800);
+
+    var weeks = Math.floor(totalPlayTime / 10080);
+    totalPlayTime -= (weeks*10080);
+
+    var days = Math.floor(totalPlayTime / 1440)
+    totalPlayTime -= (days*1440);
+
+    var hours = Math.floor(totalPlayTime / 60);
+    totalPlayTime -= (hours*60);
+
+    var minutes = totalPlayTime;
+
+
+    $("#accountExpire").append(paidUntil);
+    $("#logonCount").append(logonCount)
+    $("#gametime").append(years + " År, " +
+                          months + " Månader, " +
+                          weeks + " Veckor, " +
+                          days + " Dagar, " +
+                          hours + " Timmar, " +
+                          minutes + " Minuter");
+}
+
+
+
+
+
